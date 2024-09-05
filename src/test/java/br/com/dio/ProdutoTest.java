59c36558d6af3f5a8f75ba9b88b9ae946bc58ece
Rodrigo Gomes Bueno <rodrigobueno.ti@gmail.com>
2024-09-05 19:18:45 -0300

    Implementado classe AuthTest

--- src/test/java/br/com/dio/ProdutoTest.java
+++ src/test/java/br/com/dio/ProdutoTest.java
@@ -1,16 +1,30 @@
 package br.com.dio;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.dio.entity.Produto;
+import br.com.dio.exception.ProductPriceException;
+import br.com.dio.service.ProdutoService;
+
+@SpringBootTest
 public class ProdutoTest {
 
+	@Autowired
+	private ProdutoService produtoService;
+	
 	@Test
-	public void testeFalso() {
-
-		assertTrue(true);
-
+	public void verificaValorNegativoNoProduto() throws Exception {
+		Produto produto = new Produto();
+		
+		produto.setNome("Teste");
+		produto.setPreco(-10.00);
+		
+		assertThrows(ProductPriceException.class, () -> produtoService.save(produto));
+		
 	}
 
 }
